be5f75bc395c8349fe44cbacdd5d202590b44290
ej550 <edward.oughton@gmail.com>
2019-08-13 11:43:40 +0100

    Filter through results to get exact receiver

--- tests/test_system_simulator.py
+++ tests/test_system_simulator.py
@@ -20,8 +20,10 @@ def test_estimate_link_budget(base_system, setup_modulation_coding_lut,
         setup_simulation_parameters
         )
 
-    assert round(actual_result[0]['path_loss'], 2) == 108.99
-    assert round(actual_result[0]['received_power'], 2) == -57.99
-    assert round(actual_result[0]['sinr'], 2) == 1.87
-    assert round(actual_result[0]['capacity_mbps'], 2) == 14.77
-    assert round(actual_result[0]['capacity_mbps_km2'], 2) == 68.2
+    for receiver in actual_result:
+        if receiver['id'] == 'id_0':
+            assert round(actual_result[0]['path_loss'], 2) == 108.99
+            assert round(actual_result[0]['received_power'], 2) == -57.99
+            assert round(actual_result[0]['sinr'], 2) == 1.87
+            assert round(actual_result[0]['capacity_mbps'], 2) == 14.77
+            assert round(actual_result[0]['capacity_mbps_km2'], 2) == 68.2
