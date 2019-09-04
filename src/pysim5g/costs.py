d771c16af5380115aa3faf4032832efc07b3e460
ej550 <edward.oughton@gmail.com>
2019-09-04 13:32:23 +0100

    Add new cost strategies

--- src/pysim5g/costs.py
+++ src/pysim5g/costs.py
@@ -37,19 +37,17 @@ def calculate_costs(data, costs, simulation_parameters, site_radius, environment
         'passive_site_sharing',
         'passive_backhaul_sharing',
         'active_moran',
-        'active_mocn',
-        'active_cn_sharing',
     ]
 
     output = []
 
     for strategy in sharing_strategies:
         print('working on {}'.format(strategy))
-        for key, value in simulation_parameters.items():
-            if key == 'backhaul_distance_km_{}'.format(environment):
-                backhaul_distance = value
+        # for key, value in simulation_parameters.items():
+        #     if key == 'backhaul_distance_km_{}'.format(environment):
+        #         backhaul_distance = value
 
-        cost_breakdown = get_costs(strategy, costs, sites_per_km2, environment, backhaul_distance, simulation_parameters)
+        cost_breakdown = get_costs(strategy, costs, sites_per_km2, environment, simulation_parameters)
 
         total_deployment_costs_km2 = 0
         for key, value in cost_breakdown.items():
@@ -71,43 +69,38 @@ def calculate_costs(data, costs, simulation_parameters, site_radius, environment
                 'capacity_mbps_km2': data['capacity_mbps'],
                 'strategy': strategy,
                 'total_deployment_costs_km2': total_deployment_costs_km2,
-                'sector_antenna_costs_km2': cost_breakdown['single_sector_antenna_2x2_mimo_dual_band'],
+                'sector_antenna_costs_km2': cost_breakdown['single_sector_antenna'],
                 'remote_radio_unit_costs_km2': cost_breakdown['single_remote_radio_unit'],
                 'baseband_unit_costs_km2': cost_breakdown['single_baseband_unit'],
                 'router_costs_km2': cost_breakdown['router'],
-                'site_rental': cost_breakdown['site_rental'],
+                'site_rental_km2': cost_breakdown['site_rental'],
                 'tower_costs_km2': cost_breakdown['tower'],
                 'civil_material_costs_km2': cost_breakdown['civil_materials'],
                 'transportation_costs_km2': cost_breakdown['transportation'],
                 'installation_costs_km2': cost_breakdown['installation'],
-                'battery_system_costs_km2': cost_breakdown['battery_system'],
-                'fiber_backhaul_costs_km2': cost_breakdown['fiber_backhaul_{}'.format(environment)],
-                'microwave_backhaul_1m_costs_km2': cost_breakdown['microwave_backhaul_1m'],
+                'power_system_costs_km2': cost_breakdown['power_generator_battery_system'],
+                'fiber_backhaul_costs_km2': cost_breakdown['high_speed_backhaul_hub'],
             }
         )
 
     return output
 
 
-def get_costs(strategy, costs, sites_per_km2, environment, backhaul_distance, simulation_parameters):
+def get_costs(strategy, costs, sites_per_km2, environment, simulation_parameters):
 
     if strategy == 'baseline':
-        costs = baseline(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters)
+        costs = baseline(costs, sites_per_km2, environment, simulation_parameters)
     if strategy == 'passive_site_sharing':
-        costs = compound_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters)
+        costs = passive_site_sharing(costs, sites_per_km2, environment, simulation_parameters)
     if strategy == 'passive_backhaul_sharing':
-        costs = mast_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters)
+        costs = passive_backhaul_sharing(costs, sites_per_km2, environment, simulation_parameters)
     if strategy == 'active_moran':
-        costs = ran_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters)
-    if strategy == 'active_mocn':
-        costs = mast_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters)
-    if strategy == 'active_cn_sharing':
-        costs = ran_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters)
+        costs = active_moran(costs, sites_per_km2, environment, simulation_parameters)
 
     return costs
 
 
-def baseline(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters):
+def baseline(costs, sites_per_km2, environment, simulation_parameters):
     """
     No sharing takes place.
 
@@ -115,8 +108,8 @@ def baseline(costs, sites_per_km2, environment, backhaul_distance, simulation_pa
 
     """
     cost_breakdown = {
-        'single_sector_antenna_2x2_mimo_dual_band': (
-            costs['single_sector_antenna_2x2_mimo_dual_band'] *
+        'single_sector_antenna': (
+            costs['single_sector_antenna'] *
             simulation_parameters['sectorization'] * sites_per_km2
         ),
         'single_remote_radio_unit': (
@@ -125,60 +118,6 @@ def baseline(costs, sites_per_km2, environment, backhaul_distance, simulation_pa
         ),
         'single_baseband_unit': (
             costs['single_baseband_unit'] * sites_per_km2
-        ),
-        'router': (
-            costs['router'] * sites_per_km2
-        ),
-        'site_rental': costs['site_rental'],
-        'tower': (
-            costs['tower'] * sites_per_km2
-        ),
-        'civil_materials': (
-            costs['civil_materials'] * sites_per_km2
-        ),
-        'transportation': (
-            costs['transportation'] * sites_per_km2
-        ),
-        'installation': (
-            costs['installation'] * sites_per_km2
-        ),
-        'battery_system': (
-            costs['battery_system'] * sites_per_km2
-        ),
-        'fiber_backhaul_{}'.format(environment): (
-            costs['fixed_fiber_backhaul_per_km'] * backhaul_distance * sites_per_km2
-        ),
-        'microwave_backhaul_1m': (
-            costs['microwave_backhaul_1m'] * sites_per_km2
-        )
-    }
-
-    return cost_breakdown
-
-
-def passive_site_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters):
-    """
-    Sharing of:
-        - Site compound
-
-    """
-    cost_breakdown = {
-        'single_sector_antenna_2x2_mimo_dual_band': (
-            costs['single_sector_antenna_2x2_mimo_dual_band'] *
-            simulation_parameters['sectorization'] * sites_per_km2
-        ),
-        'single_remote_radio_unit': (
-            costs['single_remote_radio_unit'] *
-            simulation_parameters['sectorization'] * sites_per_km2
-        ),
-        'single_baseband_unit': (
-            costs['single_baseband_unit'] * sites_per_km2
-        ),
-        'router': (
-            costs['router'] * sites_per_km2
-        ),
-        'site_rental': (
-            costs['site_rental'] / simulation_parameters['number_of_mnos_sharing']
         ),
         'tower': (
             costs['tower'] * sites_per_km2
@@ -192,30 +131,32 @@ def passive_site_sharing(costs, sites_per_km2, environment, backhaul_distance, s
         'installation': (
             costs['installation'] * sites_per_km2
         ),
-        'battery_system': (
-            costs['battery_system'] * sites_per_km2
+        'site_rental': (
+            costs['site_rental'] * sites_per_km2
         ),
-        'fiber_backhaul_{}'.format(environment): (
-            costs['fixed_fiber_backhaul_per_km'] * backhaul_distance * sites_per_km2
+        'power_generator_battery_system': (
+            costs['power_generator_battery_system'] * sites_per_km2
         ),
-        'microwave_backhaul_1m': (
-            costs['microwave_backhaul_1m'] * sites_per_km2
+        'high_speed_backhaul_hub': (
+            costs['high_speed_backhaul_hub'] * sites_per_km2
+        ),
+        'router': (
+            costs['router'] * sites_per_km2
         )
     }
 
     return cost_breakdown
 
 
-def passive_backhaul_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters):
+def passive_site_sharing(costs, sites_per_km2, environment, simulation_parameters):
     """
     Sharing of:
         - Site compound
-        - Mast
 
     """
     cost_breakdown = {
-        'single_sector_antenna_2x2_mimo_dual_band': (
-            costs['single_sector_antenna_2x2_mimo_dual_band'] *
+        'single_sector_antenna': (
+            costs['single_sector_antenna'] *
             simulation_parameters['sectorization'] * sites_per_km2
         ),
         'single_remote_radio_unit': (
@@ -225,39 +166,89 @@ def passive_backhaul_sharing(costs, sites_per_km2, environment, backhaul_distanc
         'single_baseband_unit': (
             costs['single_baseband_unit'] * sites_per_km2
         ),
+        'tower': (
+            costs['tower'] * sites_per_km2 / simulation_parameters['mnos']
+        ),
+        'civil_materials': (
+            costs['civil_materials'] * sites_per_km2 / simulation_parameters['mnos']
+        ),
+        'transportation': (
+            costs['transportation'] * sites_per_km2 / simulation_parameters['mnos']
+        ),
+        'installation': (
+            costs['installation'] * sites_per_km2 / simulation_parameters['mnos']
+        ),
+        'site_rental': (
+            costs['site_rental'] * sites_per_km2 / simulation_parameters['mnos']
+        ),
+        'power_generator_battery_system': (
+            costs['power_generator_battery_system'] * sites_per_km2 /
+             simulation_parameters['mnos']
+        ),
+        'high_speed_backhaul_hub': (
+            costs['high_speed_backhaul_hub'] * sites_per_km2
+        ),
         'router': (
             costs['router'] * sites_per_km2
-        ),
-        'site_rental': (
-            costs['site_rental'] / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'tower': (
-            costs['tower'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'civil_materials': (
-            costs['civil_materials'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'transportation': (
-            costs['transportation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'installation': (
-            costs['installation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'battery_system': (
-            costs['battery_system'] * sites_per_km2
-        ),
-        'fiber_backhaul_{}'.format(environment): (
-            costs['fixed_fiber_backhaul_per_km'] * backhaul_distance * sites_per_km2
-        ),
-        'microwave_backhaul_1m': (
-            costs['microwave_backhaul_1m'] * sites_per_km2
         )
     }
 
     return cost_breakdown
 
 
-def active_moran(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters):
+def passive_backhaul_sharing(costs, sites_per_km2, environment, simulation_parameters):
+    """
+    Sharing of:
+        - Site compound
+        - Mast
+
+    """
+    cost_breakdown = {
+        'single_sector_antenna': (
+            costs['single_sector_antenna'] *
+            simulation_parameters['sectorization'] * sites_per_km2
+        ),
+        'single_remote_radio_unit': (
+            costs['single_remote_radio_unit'] *
+            simulation_parameters['sectorization'] * sites_per_km2
+        ),
+        'single_baseband_unit': (
+            costs['single_baseband_unit'] * sites_per_km2
+        ),
+        'tower': (
+            costs['tower'] * sites_per_km2
+        ),
+        'civil_materials': (
+            costs['civil_materials'] * sites_per_km2
+        ),
+        'transportation': (
+            costs['transportation'] * sites_per_km2
+        ),
+        'installation': (
+            costs['installation'] * sites_per_km2
+        ),
+        'site_rental': (
+            costs['site_rental'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'power_generator_battery_system': (
+            costs['power_generator_battery_system'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'high_speed_backhaul_hub': (
+            costs['high_speed_backhaul_hub'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'router': (
+            costs['router'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        )
+    }
+
+    return cost_breakdown
+
+
+def active_moran(costs, sites_per_km2, environment, simulation_parameters):
     """
     Sharing of:
         - RAN
@@ -266,161 +257,51 @@ def active_moran(costs, sites_per_km2, environment, backhaul_distance, simulatio
 
     """
     cost_breakdown = {
-        'single_sector_antenna_2x2_mimo_dual_band': (
-            costs['single_sector_antenna_2x2_mimo_dual_band'] *
+        'single_sector_antenna': (
+            costs['single_sector_antenna'] *
             simulation_parameters['sectorization'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
+            simulation_parameters['mnos']
         ),
         'single_remote_radio_unit': (
             costs['single_remote_radio_unit'] *
             simulation_parameters['sectorization'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
+            simulation_parameters['mnos']
         ),
         'single_baseband_unit': (
             costs['single_baseband_unit'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
+            simulation_parameters['mnos']
+        ),
+        'tower': (
+            costs['tower'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'civil_materials': (
+            costs['civil_materials'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'transportation': (
+            costs['transportation'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'installation': (
+            costs['installation'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'site_rental': (
+            costs['site_rental'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'power_generator_battery_system': (
+            costs['power_generator_battery_system'] * sites_per_km2 /
+            simulation_parameters['mnos']
+        ),
+        'high_speed_backhaul_hub': (
+            costs['high_speed_backhaul_hub'] * sites_per_km2 /
+            simulation_parameters['mnos']
         ),
         'router': (
             costs['router'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'site_rental': (
-            costs['site_rental'] / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'tower': (
-            costs['tower'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'civil_materials': (
-            costs['civil_materials'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'transportation': (
-            costs['transportation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'installation': (
-            costs['installation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'battery_system': (
-            costs['battery_system'] * sites_per_km2
-        ),
-        'fiber_backhaul_{}'.format(environment): (
-            costs['fixed_fiber_backhaul_per_km'] * backhaul_distance * sites_per_km2
-        ),
-        'microwave_backhaul_1m': (
-            costs['microwave_backhaul_1m'] * sites_per_km2
-        )
-    }
-
-    return cost_breakdown
-
-
-
-def active_mocn(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters):
-    """
-    Sharing of:
-        - RAN
-        - Mast
-        - Site compound
-
-    """
-    cost_breakdown = {
-        'single_sector_antenna_2x2_mimo_dual_band': (
-            costs['single_sector_antenna_2x2_mimo_dual_band'] *
-            simulation_parameters['sectorization'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'single_remote_radio_unit': (
-            costs['single_remote_radio_unit'] *
-            simulation_parameters['sectorization'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'single_baseband_unit': (
-            costs['single_baseband_unit'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'router': (
-            costs['router'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'site_rental': (
-            costs['site_rental'] / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'tower': (
-            costs['tower'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'civil_materials': (
-            costs['civil_materials'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'transportation': (
-            costs['transportation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'installation': (
-            costs['installation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'battery_system': (
-            costs['battery_system'] * sites_per_km2
-        ),
-        'fiber_backhaul_{}'.format(environment): (
-            costs['fixed_fiber_backhaul_per_km'] * backhaul_distance * sites_per_km2
-        ),
-        'microwave_backhaul_1m': (
-            costs['microwave_backhaul_1m'] * sites_per_km2
-        )
-    }
-
-    return cost_breakdown
-
-
-
-def active_cn_sharing(costs, sites_per_km2, environment, backhaul_distance, simulation_parameters):
-    """
-    Sharing of:
-        - RAN
-        - Mast
-        - Site compound
-
-    """
-    cost_breakdown = {
-        'single_sector_antenna_2x2_mimo_dual_band': (
-            costs['single_sector_antenna_2x2_mimo_dual_band'] *
-            simulation_parameters['sectorization'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'single_remote_radio_unit': (
-            costs['single_remote_radio_unit'] *
-            simulation_parameters['sectorization'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'single_baseband_unit': (
-            costs['single_baseband_unit'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'router': (
-            costs['router'] * sites_per_km2 /
-            simulation_parameters['number_of_mnos_sharing']
-        ),
-        'site_rental': (
-            costs['site_rental'] / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'tower': (
-            costs['tower'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'civil_materials': (
-            costs['civil_materials'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'transportation': (
-            costs['transportation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'installation': (
-            costs['installation'] * sites_per_km2 / simulation_parameters['number_of_mnos_sharing']
-        ),
-        'battery_system': (
-            costs['battery_system'] * sites_per_km2
-        ),
-        'fiber_backhaul_{}'.format(environment): (
-            costs['fixed_fiber_backhaul_per_km'] * backhaul_distance * sites_per_km2
-        ),
-        'microwave_backhaul_1m': (
-            costs['microwave_backhaul_1m'] * sites_per_km2
+            simulation_parameters['mnos']
         )
     }
 
