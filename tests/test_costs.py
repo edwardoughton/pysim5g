273fb3398e9065f49d352dc5b62c474c07e33546
ej550 <edward.oughton@gmail.com>
2019-09-05 08:55:26 +0100

    Update cost tests to reflect strategies

--- tests/test_costs.py
+++ tests/test_costs.py
@@ -9,14 +9,70 @@ def test_calculate_costs(setup_data, setup_costs, setup_simulation_parameters,
         setup_site_radius, setup_environment
     )
 
-    assert round(percentile_site_results['sector_antenna_costs_km2']) == 20785
-    assert round(percentile_site_results['remote_radio_unit_costs_km2']) == 55426
-    assert round(percentile_site_results['baseband_unit_costs_km2']) == 46188
-    assert round(percentile_site_results['router_costs_km2']) == 9238
-    assert round(percentile_site_results['tower_costs_km2']) == 46188
-    assert round(percentile_site_results['civil_material_costs_km2']) == 23094
-    assert round(percentile_site_results['transportation_costs_km2']) == 46188
-    assert round(percentile_site_results['installation_costs_km2']) == 23094
-    assert round(percentile_site_results['battery_system_costs_km2']) == 36950
-    assert round(percentile_site_results['fiber_backhaul_costs_km2']) ==  69282
-    assert round(percentile_site_results['microwave_backhaul_1m_costs_km2']) == 18475
+    baseline_results = []
+    for result in percentile_site_results:
+        if result['strategy'] == 'baseline':
+            baseline_results.append(result)
+
+    assert round(baseline_results[0]['sector_antenna_costs_km2']) == 20785
+    assert round(baseline_results[0]['remote_radio_unit_costs_km2']) == 55426
+    assert round(baseline_results[0]['baseband_unit_costs_km2']) == 46188
+    assert round(baseline_results[0]['tower_costs_km2']) == 46188
+    assert round(baseline_results[0]['civil_material_costs_km2']) == 23094
+    assert round(baseline_results[0]['transportation_costs_km2']) == 46188
+    assert round(baseline_results[0]['installation_costs_km2']) == 23094
+    assert round(baseline_results[0]['site_rental_km2']) == 69282
+    assert round(baseline_results[0]['power_system_costs_km2']) == 23094
+    assert round(baseline_results[0]['fiber_backhaul_costs_km2']) ==  69282
+    assert round(baseline_results[0]['router_costs_km2']) == 9238
+
+    passive_site_sharing = []
+    for result in percentile_site_results:
+        if result['strategy'] == 'passive_site_sharing':
+            passive_site_sharing.append(result)
+
+    assert round(passive_site_sharing[0]['sector_antenna_costs_km2']) == 20785
+    assert round(passive_site_sharing[0]['remote_radio_unit_costs_km2']) == 55426
+    assert round(passive_site_sharing[0]['baseband_unit_costs_km2']) == 46188
+    assert round(passive_site_sharing[0]['tower_costs_km2']) == 23094
+    assert round(passive_site_sharing[0]['civil_material_costs_km2']) == 11547
+    assert round(passive_site_sharing[0]['transportation_costs_km2']) == 23094
+    assert round(passive_site_sharing[0]['installation_costs_km2']) == 11547
+    assert round(passive_site_sharing[0]['site_rental_km2']) == 34641
+    assert round(passive_site_sharing[0]['power_system_costs_km2']) == 11547
+    assert round(passive_site_sharing[0]['fiber_backhaul_costs_km2']) ==  69282
+    assert round(passive_site_sharing[0]['router_costs_km2']) == 9238
+
+    passive_backhaul_sharing = []
+    for result in percentile_site_results:
+        if result['strategy'] == 'passive_backhaul_sharing':
+            passive_backhaul_sharing.append(result)
+
+    assert round(passive_backhaul_sharing[0]['sector_antenna_costs_km2']) == 20785
+    assert round(passive_backhaul_sharing[0]['remote_radio_unit_costs_km2']) == 55426
+    assert round(passive_backhaul_sharing[0]['baseband_unit_costs_km2']) == 46188
+    assert round(passive_backhaul_sharing[0]['tower_costs_km2']) == 46188
+    assert round(passive_backhaul_sharing[0]['civil_material_costs_km2']) == 23094
+    assert round(passive_backhaul_sharing[0]['transportation_costs_km2']) == 46188
+    assert round(passive_backhaul_sharing[0]['installation_costs_km2']) == 23094
+    assert round(passive_backhaul_sharing[0]['site_rental_km2']) == 34641
+    assert round(passive_backhaul_sharing[0]['power_system_costs_km2']) == 11547
+    assert round(passive_backhaul_sharing[0]['fiber_backhaul_costs_km2']) ==  34641
+    assert round(passive_backhaul_sharing[0]['router_costs_km2']) == 4619
+
+    active_moran = []
+    for result in percentile_site_results:
+        if result['strategy'] == 'active_moran':
+            active_moran.append(result)
+
+    assert round(active_moran[0]['sector_antenna_costs_km2']) == 10392
+    assert round(active_moran[0]['remote_radio_unit_costs_km2']) == 27713
+    assert round(active_moran[0]['baseband_unit_costs_km2']) == 23094
+    assert round(active_moran[0]['tower_costs_km2']) == 23094
+    assert round(active_moran[0]['civil_material_costs_km2']) == 11547
+    assert round(active_moran[0]['transportation_costs_km2']) == 23094
+    assert round(active_moran[0]['installation_costs_km2']) == 11547
+    assert round(active_moran[0]['site_rental_km2']) == 34641
+    assert round(active_moran[0]['power_system_costs_km2']) == 11547
+    assert round(active_moran[0]['fiber_backhaul_costs_km2']) ==  34641
+    assert round(active_moran[0]['router_costs_km2']) == 4619
